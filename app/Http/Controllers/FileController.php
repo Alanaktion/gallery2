488b624777f7e8dd55520489160fe15c8b0e20aa
Alan Hardman <alanaktion@gmail.com>
2021-01-31 23:39:42 -0700

    Handle range requests

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -17,14 +17,66 @@ class FileController extends Controller
 
     public function proxy(string $file)
     {
-        // TODO: support HTTP range requests for video buffering
         $path = config('gallery.path') . $file;
         if (!is_file($path)) {
             return abort(404);
         }
+
+        // https://gist.github.com/codler/3906826
+        $fp = @fopen($path, 'rb');
+        $size = filesize($path);
+        $length = $size;
+        $start = 0;
+        $end = $size - 1;
+
         $mime = mime_content_type($path);
         header('Content-Type: ' . $mime);
-        readfile($path);
+        header('Accept-Range: bytes');
+
+        if (isset($_SERVER['HTTP_RANGE'])) {
+            $c_start = $start;
+            $c_end = $end;
+
+            list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
+            if (strpos($range, ',') !== false) {
+                header('HTTP/1.1 416 Requested Range Not Satisfiable');
+                header("Content-Range: bytes $start-$end/$size");
+                exit;
+            }
+            if ($range == '-') {
+                $c_start = $size - substr($range, 1);
+            } else {
+                $range = explode('-', $range);
+                $c_start = $range[0];
+                $c_end = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $size;
+            }
+            $c_end = ($c_end > $end) ? $end : $c_end;
+            if ($c_start > $c_end || $c_start > $size - 1 || $c_end >= $size) {
+                header('HTTP/1.1 416 Requested Range Not Satisfiable');
+                header("Content-Range: bytes $start-$end/$size");
+                exit;
+            }
+            $start = $c_start;
+            $end = $c_end;
+            $length = $end - $start + 1;
+            fseek($fp, $start);
+            header('HTTP/1.1 206 Partial Content');
+        }
+
+        header("Content-Range: bytes $start-$end/$size");
+        header("Content-Length: $length");
+
+        $buffer = 1024 * 8;
+        while (!feof($fp) && ($p = ftell($fp)) <= $end) {
+            if ($p + $buffer > $end) {
+                $buffer = $end - $p + 1;
+            }
+            set_time_limit(0);
+            echo fread($fp, $buffer);
+            flush();
+        }
+
+        fclose($fp);
     }
 
     public function thumbnail(string $scale, string $file)
