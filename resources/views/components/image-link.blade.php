8be4a1f1a60f667bdd77d4a2e4be612c6dc71edb
Alan Hardman <alanaktion@gmail.com>
2021-02-01 12:02:19 -0700

    Fix video overlay

--- resources/views/components/image-link.blade.php
+++ resources/views/components/image-link.blade.php
@@ -11,7 +11,7 @@
         <div class="p-1 absolute bottom-0 left-0 right-0 text-sm bg-white bg-opacity-75 backdrop-blur dark:bg-black opacity-0 group-hover:opacity-100 overflow-hidden overflow-ellipsis whitespace-nowrap z-10">
             {{ $file['name'] }}
         </div>
-        @if ($type == 'video' || $type == 'image')
+        @if ($type == 'video')
             <div class="p-1 absolute top-1 right-1 rounded-full bg-white bg-opacity-75 backdrop-blur dark:bg-black">
                 <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"></path>
